c8f484a37b764f70625ef91f634d0eaaa788cc65
Jolka <firjola@gmail.com>
2021-11-29 17:24:42 +0100

    Updates

---
+++ precache-manifest.aa5bd70daf1a838cd4761175169a7d2e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b032a17d559b235769bfb4abe74e3b2",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "8ec09d00a687dd28d61d",
+    "url": "/react-form/static/js/2.cce7aa39.chunk.js"
+  },
+  {
+    "revision": "82a7fb9b0fb6cd8fbce1",
+    "url": "/react-form/static/js/main.f0c9e7ba.chunk.js"
+  },
+  {
+    "revision": "15bae7cbdef7d06c46be",
+    "url": "/react-form/static/js/runtime~main.98e38a88.js"
+  }
+]);
